9f5a9d11b8eade0e277e880f2657ea829ad90e20
Thezzer <miguelarcanjocavallimedeiros@gmail.com>
2026-03-10 20:37:00 -0300

    exercicio 6

--- phpRep/exercicio5rep.php
+++ phpRep/exercicio5rep.php
@@ -36,45 +36,49 @@
                         <div class="mt-5">
                             <div class="text-center">
                                 <?php
-                                    $mes = $_POST["mes"];
 
-                                    switch ($mes) {
-                                        case "1":
-                                            echo"1 - Janeiro";
-                                            break;
-                                        case "2":
-                                            echo "2 - Feveiro";
-                                            break;
-                                        case "3":
-                                            echo "3 - Março";
-                                            break;
-                                        case "4":
-                                            echo "4 - Abril";
-                                            break;
-                                        case "5":
-                                            echo "5 - Maio";
-                                            break;
-                                        case "6":
-                                            echo "6 - Junho";
-                                            break;
-                                        case "7":
-                                            echo "7 - Julho";
-                                            break;
-                                        case "8":
-                                            echo "8 - Agosto";
-                                            break;
-                                        case "9":
-                                            echo "9 - Setembro";
-                                            break;
-                                        case "10":
-                                            echo "10 - Outubro";
-                                            break;
-                                        case "11":
-                                            echo "11 - Novembro";
-                                            break;
-                                        case "12":
-                                            echo "12 - Dezembro";
-                                            break;
+                                    if($_SERVER["REQUEST_METHOD"] == "POST"){
+                                        $mes = $_POST["mes"];
+
+                                        switch ($mes) {
+                                            case "1":
+                                                echo"1 - Janeiro";
+                                                break;
+                                            case "2":
+                                                echo "2 - Feveiro";
+                                                break;
+                                            case "3":
+                                                echo "3 - Março";
+                                                break;
+                                            case "4":
+                                                echo "4 - Abril";
+                                                break;
+                                            case "5":
+                                                echo "5 - Maio";
+                                                break;
+                                            case "6":
+                                                echo "6 - Junho";
+                                                break;
+                                            case "7":
+                                                echo "7 - Julho";
+                                                break;
+                                            case "8":
+                                                echo "8 - Agosto";
+                                                break;
+                                            case "9":
+                                                echo "9 - Setembro";
+                                                break;
+                                            case "10":
+                                                echo "10 - Outubro";
+                                                break;
+                                            case "11":
+                                                echo "11 - Novembro";
+                                                break;
+                                            case "12":
+                                                echo "12 - Dezembro";
+                                                break;
+                                        }
+
                                     }
                                     
                                 ?>
