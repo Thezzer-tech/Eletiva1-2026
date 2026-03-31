be199e25b362b194b226a286ef15596aac018e4b
miguel <miguelarcanjocavallimedeiros@gmail.com>
2026-03-31 20:40:43 -0300

    exercicio 5

---
+++ ExercicioArray/exercicio5array.php
@@ -0,0 +1,100 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width" initial-scale="1.0">
+    <title>Exercicio 2 rep</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+
+    <style>
+    .cordefundo {
+        background-color: #f3f2f4;
+    }
+    </style>
+</head>
+
+<body>
+
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card shadow-sm">
+                    <div class="card-body p-4 cordefundo">
+                        <form class="row" method="post">
+                            <?php
+                            for($i = 0; $i < 5; $i++)
+                                echo '<div class="col-md-6 pt-3">
+                                    <label class="form-label" for="nomes[]">Informe Titulo: </label>
+                                    <input type="text" id="nomes[]" name="nomes[]" class="form-control">
+                                </div>
+                                <div class="col-md-3 pt-3">
+                                    <label class="form-label" for="qntd[]">Informe Quantidade: </label>
+                                    <input type="number" id="qntd[]" name="qntd[]" class="form-control">
+                                    </div>';
+                            ?>
+
+                            <div class="mt-5">
+                                    <div class="text-end">
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                        <button type="reset" class="btn btn-light">Cancel</button>
+                                    </div>
+                            </div>
+
+                        </form>
+                        <div class="mt-5">
+                            <div class="text-center">
+                                <?php
+                                    if($_SERVER["REQUEST_METHOD"] == "POST"){
+                                        $nome = $_POST["nomes"];
+                                        $qntd = $_POST["qtnd"];
+
+                                        $mapa_livros = array();
+
+                                        for($i = 0; $i < 5; $i++){
+                                            $nome_atual = $nomes[$i];
+                                            $qntd_atual = $qntd[$i];
+
+                                            $mapa_livros[$nome_atual] = $qntd_atual;
+                                        }
+
+
+                                        foreach($mapa_livros as $nome => $quantia){
+
+                                            if($primeiro && $quantia < 5 ){
+                                                echo "<p>livro em baixa quantidade!!</p>";
+                                                echo "<p>Livro: ". $nome . " Quantidade: " . $quantia . "</p>";
+                                                $primeiro = false;
+                                            }
+                                            elseif($quantia < 5){
+                                                echo "<p>Livro: ". $nome . " Quantidade: " . $qntd . "</p>";
+                                            }
+
+                                        }
+
+                                        $primeiro = true;
+
+                                        foreach($mapa_livros as $nome => $quantia){
+                                            if($primeiro && $quantia >= 5){
+                                                echo "<p>livros Regulares!</p>";
+                                                echo "<p>Livro: ". $nome . " Quantidade: " . $quantia . "</p>"; 
+
+                                                $primeiro = false;
+                                            }
+                                            elseif($quantia >= 5){
+                                                echo "<p>Livro: ". $nome . " Quantidade: " . $quantia . "</p>";
+                                            }
+                                        }
+
+                                        
+                                    }
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
