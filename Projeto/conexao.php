2105f1eeeb3e2f530446ea134b09ce4561f91b02
miguel <miguelarcanjocavallimedeiros@gmail.com>
2026-04-14 20:20:10 -0300

    Ajuste

--- Projeto/conexao.php
+++ Projeto/conexao.php
@@ -1,11 +1,14 @@
 <?php
 
-    $dominio = "mysql:host=localhost;dbname=projetophp";
+    $dominio = "mysql:host=localhost;dbneme=projetophp";
     $usuario = "root";
     $senha = "";
 
+
     try {
         $pdo = new PDO($dominio, $usuario, $senha);
-    } catch (Exception $e) {
-        die("Erro ao conectar ao banco!".$e->getMessage());
+    }catch (Exception $e){
+        die("Erro ao conectar ao banco: " . $e->getMessage());
     }
+
+?>
